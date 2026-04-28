2abcc2ce7583f2faf642702e0e863025bf7f03cb
sarvjeetsinghkashyap <sarvjeetsinghkashyap@gmail.com>
2026-04-29 01:04:09 +0530

    Saved state for layout Web

---
+++ resources/views/layouts/web.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="css/global.css">
+    <link rel="stylesheet" href="css/web.css">
+</head>
+<body>
+    <nav>Nav</nav>
+    <header>
+        <video src="assets/kyn-ai-landing-hero.mp4" autoplay loop muted></video>
+    </header>
+    <main></main>
+</body>
+</html>
