fa2e762a3f16b3e4b5b0652d294990d7efafe888
sarvjeetsinghkashyap <sarvjeetsinghkashyap@gmail.com>
2026-04-29 17:26:43 +0530

    Update UI

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -10,7 +10,22 @@
     <link rel="stylesheet" href="css/web.css">
 </head>
 <body>
-    <nav>Nav</nav>
+    <nav>
+        <div class="row">
+            <div class="col-1 left">
+                <img src="assets/logo.jpg" alt="logo">
+            </div>
+            <div class="col-2 center">
+                <ul class="menu">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Services</a></li>
+                    <li><a href="">About Us</a></li>
+                    <li><a href="">Contact Us</a></li>
+                </ul>
+            </div>
+            <div class="col-1 right"></div>
+        </div>
+    </nav>
     <header>
         <video src="assets/kyn-ai-landing-hero.mp4" autoplay loop muted></video>
     </header>
