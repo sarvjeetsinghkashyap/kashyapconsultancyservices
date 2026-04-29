912976f6e9aa1a06fbbc37f7fdf610fabe942206
sarvjeetsinghkashyap <sarvjeetsinghkashyap@gmail.com>
2026-04-29 23:23:18 +0530

    Updated nav

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -13,22 +13,48 @@
     <nav>
         <div class="row">
             <div class="col-1 left">
-                <img src="assets/logo.jpg" alt="logo">
+                <img src="assets/logo.jpg" alt="logo">&nbsp
+                <span>Kashyap<br>Consultancy Services</span>
             </div>
-            <div class="col-2 center">
+            <div class="col-2 right">
                 <ul class="menu">
                     <li><a href="">Home</a></li>
                     <li><a href="">Services</a></li>
                     <li><a href="">About Us</a></li>
                     <li><a href="">Contact Us</a></li>
                 </ul>
+                <button class="btn">Login</button>&nbsp
+                <button class="btn-alt">Sign Up</button>
             </div>
-            <div class="col-1 right"></div>
+            {{-- <div class="col-1 right">
+                <button class="btn">Login</button>&nbsp
+                <button class="btn-alt">Sign Up</button>
+            </div> --}}
         </div>
     </nav>
     <header>
         <video src="assets/kyn-ai-landing-hero.mp4" autoplay loop muted></video>
+        <div class="header-overlay">
+            <h1>sdhfndsifo</h1>
+        </div>
     </header>
     <main></main>
 </body>
+<script src="scripts/jquery.js"></script>
+<script>
+    $(document).ready(function(){
+        $(window).on("scroll", function() {
+            var vh = $(window).height() - 60;
+            if ($(window).scrollTop() > vh) {
+                $("nav").css({
+                    "background": "#fff"
+                });
+            } else {
+                $("nav").css({
+                    "background": "transparent"
+                });
+            }
+        });
+    });
+</script>
 </html>
