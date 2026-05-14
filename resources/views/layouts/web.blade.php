7729bc061fdd639446c345eb9dccc93bad347734
sarvjeetsinghkashyap <sarvjeetsinghkashyap@gmail.com>
2026-05-14 14:36:28 +0530

    UI Update

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -16,9 +16,9 @@
                 <img src="../assets/emb.png" alt="logo">
             </div>
             <ul class="col-2 nav-links">
-                <li><a href="">Home</a></li>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#services">Services</a></li>
                 <li><a href="">About</a></li>
-                <li><a href="">Services</a></li>
                 <li><a href="">Contact</a></li>
             </ul>
             <span class="toggle-menu menu-show">☰</span>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.web')
 @section('title', 'Home: Kashyap Consultancy Services')
 @section('content')
-    <header>
+    <header id="home">
         <video src="assets/kyn-ai-landing-hero.mp4" autoplay loop muted></video>
         <div class="overlay">
             <h1>Kashyap Consultancy Services</h1>
@@ -9,4 +9,27 @@
             <a href="" class="btn-cta">Get Started</a>
         </div>
     </header>
+    <section id="services">
+        <div class="row mt-100">
+            <div class="col">
+                <span class="heading">Our Services</span>
+            </div>
+        </div>
+        <div class="card-holder">
+            <div class="card mt-100">
+                <img class="main-img" src="assets/image_0429.png" alt="card-header-img">
+                <div class="card-heading">Management and Support Services</div>
+                <div class="card-description">Optimising daily operations with scalable administrative support, resource management, and strategic oversight to drive business growth.</div>
+                <div class="card-action mt-100">
+                    <a href="">Read more</a>
+                </div>
+            </div>
+            <div class="card mt-100">
+                <img class="main-img" src="assets/image_0430.png" alt="card-header-img">
+            </div>
+            <div class="card mt-100">
+                <img class="main-img" src="assets/image_0431.png" alt="card-header-img">
+            </div>
+        </div>
+    </section>
 @endsection
